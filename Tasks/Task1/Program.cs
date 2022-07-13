13cd515eceae9175787e413a08d78a1972a5718d
Dmitryi <dima2581992@mail.ru>
2022-07-13 20:13:27 +0300

    Create

---
+++ Tasks/Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+int a = Prompt("Задайте количество чисел массива => ");
+int[] ArrayNumbers = new int[a];
+
+void InputNumbers(int a)
+{
+for (int i = 0; i < a; i++)
+  {
+    System.Console.Write($"Введите {i+1} число: ");
+    ArrayNumbers[i] = int.Parse(Console.ReadLine());
+  }
+}
+
+
+int EvenNumb(int[] ArrayNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < ArrayNumbers.Length; i++)
+  {
+    if(ArrayNumbers[i] > 0 ) 
+    count += 1; 
+  }
+  return count;
+}
+
+InputNumbers(a);
+
+System.Console.WriteLine($"Введено чисел больше 0 - {EvenNumb(ArrayNumbers)} ");
+
+
+
